2779c9f21220b4a3ef206e8ea88ea25b681f2d22
Jörg Richter <jri@deepamehta.de>
2022-07-19 14:46:14 +0200

    Fix: image URLs in translated text
    See dmx-projects/zukunftswerk#144

--- src/main/java/systems/dmx/deepl/DeepLPlugin.java
+++ src/main/java/systems/dmx/deepl/DeepLPlugin.java
@@ -34,6 +34,9 @@ public class DeepLPlugin extends PluginActivator implements DeepLService {
     private static final String DEEPL_URL = "https://api-free.deepl.com/v2/";
     private static final String DEEPL_AUTH_KEY = System.getProperty("dmx.deepl.auth_key");
 
+    private static final String IMG_START = "<img src=\"";
+    private static final String IMG_END = "\">";
+
     // ---------------------------------------------------------------------------------------------- Instance Variables
 
     private Logger logger = Logger.getLogger(getClass().getName());
@@ -53,7 +56,7 @@ public class DeepLPlugin extends PluginActivator implements DeepLService {
             // and sets method to "POST" automatically
             OutputStreamWriter out = new OutputStreamWriter(con.getOutputStream());
             out.write("text=" + text + "&target_lang=" + targetLang + "&tag_handling=xml");
-            out.flush();    // FIXME: URLEncoder.encode(text, "UTF-8")?
+            out.flush();
             String responseData = JavaUtils.readText(con.getInputStream());
             logger.info("responseData=" + responseData);
             // parse response
@@ -62,7 +65,7 @@ public class DeepLPlugin extends PluginActivator implements DeepLService {
             for (int i = 0; i < translations.length(); i++) {
                 JSONObject translation = translations.getJSONObject(i);
                 result.add(new Translation(
-                    translation.getString("text"),
+                    repairTranslation(translation.getString("text"), text),
                     translation.getString("detected_source_language")
                 ));
             }
@@ -79,11 +82,42 @@ public class DeepLPlugin extends PluginActivator implements DeepLService {
     public InputStream usageStats() {
         try {
             URLConnection con = new URL(DEEPL_URL + "usage").openConnection();
-            con.addRequestProperty("Authorization", "DeepL-Auth-Key " + DEEPL_AUTH_KEY);
+            con.setRequestProperty("Authorization", "DeepL-Auth-Key " + DEEPL_AUTH_KEY);
             con.connect();
             return con.getInputStream();
         } catch (Exception e) {
             throw new RuntimeException("Getting usage stats failed", e);
+        }
+    }
+
+    // ------------------------------------------------------------------------------------------------- Private Methods
+
+    // Note: image tag data-urls are mangled by DeepL: '+' characters are (accidentally) stripped.
+    private String repairTranslation(String translation, String text) {
+        try {
+            StringBuilder repaired = new StringBuilder();
+            int ti = 0;
+            int ri = 0;
+            int t1;
+            int count = 0;
+            while ((t1 = text.indexOf(IMG_START, ti)) != -1) {
+                int t2 = text.indexOf(IMG_END, ti + IMG_START.length());
+                int r1 = translation.indexOf(IMG_START, ri);
+                int r2 = translation.indexOf(IMG_END, ri + IMG_START.length());
+                if (r1 == -1 || r2 == -1) {
+                    throw new RuntimeException("not found in translation");
+                }
+                repaired.append(translation.substring(ri, r1));
+                repaired.append(text.substring(t1, t2 + IMG_END.length()));
+                ti = t2 + IMG_END.length();
+                ri = r2 + IMG_END.length();
+                count++;
+            }
+            repaired.append(translation.substring(ri));
+            logger.info("Image URLs repaired: " + count);
+            return repaired.toString();
+        } catch (Exception e) {
+            throw new RuntimeException("Repairing image tags failed", e);
         }
     }
 }
